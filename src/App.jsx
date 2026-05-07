3bcd6cd2fd8e9123bfddf50c9866d6f39d93c642
missonzito <missonzito@outlook.com>
2026-05-07 01:19:46 -0300

    instalação de dependencia

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Reunioes from "./pages/Reunioes";
 import ProjetoCrianca from "./pages/projetoCrianca";
 
