8c4aa79caaf6a7174f47ca1a92342355b6e3a5f0
missonzito <missonzito@outlook.com>
2026-05-19 13:19:02 -0300

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
+import { Routes, Route } from "react-router-dom";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Reunioes from "./pages/Reunioes";
 import ProjetoCrianca from "./pages/ProjetoCrianca";
@@ -14,17 +14,36 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="reunioes" element={<Reunioes />} />
-      <Route path="projeto-crianca" element={<ProjetoCrianca />} />
-      <Route path="/associacao" element={<Associacao />} />
+
+      <Route path="/reunioes" element={<Reunioes />} />
+
+      <Route
+        path="/projeto-crianca"
+        element={<ProjetoCrianca />}
+      />
+
+      <Route
+        path="/associacao"
+        element={<Associacao />}
+      />
+
       <Route path="/horta" element={<Horta />} />
-      <Route path="avisos" element={<Avisos />} />
-      <Route path="saude" element={<Saude />} />
-      <Route path="projeto-pascoa" element={<ProjetoPascoa />} />
-      <Route path="marmitas" element={<Marmitas />} />
+
+      <Route path="/avisos" element={<Avisos />} />
+
+      <Route path="/saude" element={<Saude />} />
+
+      <Route
+        path="/projetopascoa"
+        element={<ProjetoPascoa />}
+      />
+
+      <Route
+        path="/marmitas"
+        element={<Marmitas />}
+      />
     </Routes>
   );
-  
 }
 
 export default App;
